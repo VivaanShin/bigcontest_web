c2d2345cd2a5c8470c2231223f41e0d763b8a05d
VivaanShin <xhatmtjfla23@gmail.com>
2020-09-26 12:51:39 +0900

    yj update

--- public/js/demo/chart-bar-demo.js
+++ public/js/demo/chart-bar-demo.js
@@ -109,3 +109,86 @@ var myBarChart = new Chart(ctx, {
     },
   }
 });
+
+// Bar Chart Example
+var ctx = document.getElementById("myBarChart2");
+var myBarChart2 = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ["09/07", "09/08", "09/09", "09/10", "09/11", "09/12", "09/13"],
+    datasets: [{
+      label: "일별 확진자 수",
+      backgroundColor: "#4e73df",
+      hoverBackgroundColor: "#2e59d9",
+      borderColor: "#4e73df",
+      data: [119, 136, 156, 155, 176, 136, 121],
+    }],
+  },
+  options: {
+    maintainAspectRatio: false,
+    layout: {
+      padding: {
+        left: 10,
+        right: 25,
+        top: 25,
+        bottom: 0
+      }
+    },
+    scales: {
+      xAxes: [{
+        time: {
+          unit: 'month'
+        },
+        gridLines: {
+          display: false,
+          drawBorder: false
+        },
+        ticks: {
+          maxTicksLimit: 6
+        },
+        maxBarThickness: 25,
+      }],
+      yAxes: [{
+        ticks: {
+          min: 0,
+          max: 15000,
+          maxTicksLimit: 5,
+          padding: 10,
+          // Include a dollar sign in the ticks
+          callback: function(value, index, values) {
+            return '$' + number_format(value);
+          }
+        },
+        gridLines: {
+          color: "rgb(234, 236, 244)",
+          zeroLineColor: "rgb(234, 236, 244)",
+          drawBorder: false,
+          borderDash: [2],
+          zeroLineBorderDash: [2]
+        }
+      }],
+    },
+    legend: {
+      display: false
+    },
+    tooltips: {
+      titleMarginBottom: 10,
+      titleFontColor: '#6e707e',
+      titleFontSize: 14,
+      backgroundColor: "rgb(255,255,255)",
+      bodyFontColor: "#858796",
+      borderColor: '#dddfeb',
+      borderWidth: 1,
+      xPadding: 15,
+      yPadding: 15,
+      displayColors: false,
+      caretPadding: 10,
+      callbacks: {
+        label: function(tooltipItem, chart) {
+          var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
+          return datasetLabel + ': $' + number_format(tooltipItem.yLabel);
+        }
+      }
+    },
+  }
+});
--- routes/trend.js
+++ routes/trend.js
@@ -2,8 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+//trend
 router.get('/', function(req, res, next) {
-  res.render('trend', { title: 'Express' });
+  res.render('trend_population', { title: 'Express' });
 });
 
+router.get('/population', function(req, res, next) {
+  res.render('trend_population', { title: 'Express' });
+});
+
+router.get('/logistics', function(req, res, next) {
+  res.render('trend_logistics', { title: 'Express' });
+});
+
+router.get('/retail', function(req, res, next) {
+  res.render('trend_retail', { title: 'Express' });
+});
+
+router.get('/card', function(req, res, next) {
+  res.render('trend_card', { title: 'Express' });
+});
 module.exports = router;
